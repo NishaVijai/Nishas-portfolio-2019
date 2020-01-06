5fd6345d5fb12d83a0c6817bf59c8becb6a4b4da
Nisha <nishavijai@gmail.com>
2020-01-06 12:24:35 +0100

    ADD MENU COMPONENT
    Add menu component and remove nav component and its style

--- src/assets/components/Main.js
+++ src/assets/components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Nav } from './Nav';
+// import { Nav } from './Nav';
+import Menu from '../components/Menubar/Menu';
 import '../styles/main.css';
 export class Main extends React.Component {
 	render() {
 		return (
 			<div className="mainContainer">
 				<div>
-					<Nav />
+					{/* <Nav /> */}
+					<Menu />
 				</div>
 			</div>
 		);
---
+++ src/assets/components/Menubar/Menu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const toolbar = props => (
+    <header>
+        <nav>
+            <div>
+
+            </div>
+
+            <div><a href="">Logo</a></div>
+
+            <div>
+                <ul>
+                    <li><a href="">CV</a></li>
+                    <li><a href="">Projects</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default toolbar;
