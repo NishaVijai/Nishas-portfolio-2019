0e7fca56f02ca75b25c40b8e777aae44afd7f4da
Nisha <nishavijai@gmail.com>
2020-02-06 16:03:36 +0100

    ADD RESUME DESIGN
    Update resume component design

--- src/assets/components/pages/Resume/Resume.js
+++ src/assets/components/pages/Resume/Resume.js
@@ -7,35 +7,87 @@ export default class Resume extends Component {
 			<div className="resume-container">
 				<div className="resume-left">
 					<div className="personal">
-						<h4>personal</h4>
+						<h3>personal</h3>
 						<p>
-							self-taught front-end developer with no prior experience. made few projects and obtained
+							self-taught front-end developer with no prior experience. Made few projects and obtained
 							certificates from{' '}
 							<a href="https://chingu.io/" target="_blank" rel="noopener  noreferrer">
 								CHINGU-voyage
 							</a>. At present learning advanced css and working on small projects to add on my portfolio.
 						</p>
 					</div>
-					<div className="design">
+					{/* <div className="design">
 						<h4>design</h4>
 						<p>Self-taught</p>
-					</div>
+					</div> */}
+
+					<hr />
+
 					<div className="front-end">
-						<h4>front-end</h4>
-						<p>Self-taught</p>
+						<h3>front-end</h3>
+						<div className="front-end-lang">
+							<p>html</p>
+							<div className="side-line">
+								<div className="fill-in fill-80">80%</div>
+							</div>
+
+							<p>css</p>
+							<div className="side-line">
+								<div className="fill-in fill-70">70%</div>
+							</div>
+
+							<p>javascript</p>
+							<div className="side-line">
+								<div className="fill-in fill-60">60%</div>
+							</div>
+
+							<p>react</p>
+							<div className="side-line">
+								<div className="fill-in">50%</div>
+							</div>
+						</div>
 					</div>
+
+					<hr />
+
 					<div className="back-end">
-						<h4>back-end</h4>
-						<p>Self-taught</p>
+						<h3>back-end</h3>
+						<div className="back-end-lang">
+							<p>python</p>
+							<div className="side-line">
+								<div className="fill-in">50%</div>
+							</div>
+
+							<p>r</p>
+							<div className="side-line">
+								<div className="fill-in fill-30">30%</div>
+							</div>
+						</div>
 					</div>
+
+					<hr />
 				</div>
 
 				<div className="resume-right">
+					<div className="empty" />
 					<div className="first">
 						<div className="title">
 							<h2>front-end web developer</h2>
-							<address>Skibsegen 96</address>
+							<p>
+								Address: <address>Skibsegen 96, 3070 Snekkersten</address>
+							</p>
 
+							<p>
+								Mail: <a href="mailto:nishavijai@gmail.com">nishavijai@gmail.com</a>
+							</p>
+
+							<p>
+								Mobile: <a href="tel:+45-53265717">+45-53265717</a>
+							</p>
+
+							<p>
+								Website: <a href="https://nishasportfolio.netlify.com/">nishasportfolio</a>
+							</p>
 							<h4>experience</h4>
 							<p>Self-taught</p>
 						</div>
