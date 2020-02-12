d0ce47f75595446c0a180328f3e83c8cf326b128
Nisha <nishavijai@gmail.com>
2020-02-12 15:49:18 +0100

    UPDATE CONTACT COMPONENT
    Add contact component and its initial design

--- src/assets/components/pages/Contact/Contact.js
+++ src/assets/components/pages/Contact/Contact.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-
+import '../Contact/contact.css';
 export default class Contact extends Component {
 	render() {
 		return (
-			<div>
-				<div>
-					<ul>
-						<li>
-							<a href="#">G-mail</a>
-						</li>
-						<li>
-							<a href="#">Twitter</a>
-						</li>
-						<li>
-							<a href="#">LinkedIn</a>
-						</li>
-					</ul>
+			<div className="contact-container">
+				<h2>contact</h2>
+
+				<div className="form-container">
+					{/* <h2>contact</h2> */}
+					<form action="">
+						<input type="text" name="name" placeholder="Name" required />
+
+						<input type="email" name="email" placeholder="Enter E-mail" required />
+
+						<textarea type="text" name="message" placeholder="Your Message" />
+						<button>submit</button>
+					</form>
 				</div>
 			</div>
 		);
