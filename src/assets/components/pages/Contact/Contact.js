13d116438e19a0182fc2cda764d8e7f9392c0f01
Nisha <nishavijai@gmail.com>
2020-02-17 15:31:12 +0100

    UPDATE CONTACT COMPONENT
    Update contact component by adding form component and its design

--- src/assets/components/pages/Contact/Contact.js
+++ src/assets/components/pages/Contact/Contact.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
 import '../Contact/contact.css';
+import Form from '../Contact/Form';
 export default class Contact extends Component {
 	render() {
 		return (
 			<div className="contact-container">
 				<h2>contact</h2>
-
-				<div className="form-container">
-					{/* <h2>contact</h2> */}
-					<form action="">
-						<input type="text" name="name" placeholder="Name" required />
-
-						<input type="email" name="email" placeholder="Enter E-mail" required />
-
-						<textarea type="text" name="message" placeholder="Your Message" />
-						<button>submit</button>
-					</form>
-				</div>
+				<Form />
 			</div>
 		);
 	}
