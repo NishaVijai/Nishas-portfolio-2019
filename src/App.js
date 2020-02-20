d7357bcc90aef20a896453bdb2fe5e857e275ac8
Nisha <nishavijai@gmail.com>
2020-02-20 12:14:53 +0100

    UPDATE DARK LIGHT SCHEME
    Add dark light scheme in app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Nav from './assets/components/pages/Nav/Nav';
@@ -10,13 +10,76 @@ import Contact from './assets/components/pages/Contact/Contact';
 import NotFoundPage from './assets/components/pages/PageNotFound/404';
 import Footer from './assets/components/pages/Footer/Footer';
 
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './assets/components/pages/Theme';
+import { GlobalStyles } from './assets/components/pages/Global';
+
 import './App.css';
-class App extends React.Component {
-	render() {
-		return (
-			<Router>
-				<div className="main-app-container">
+import './assets/components/pages/lightMode.css';
+// class App extends React.Component {
+// 	render() {
+function App() {
+	// const [ theme, setTheme ] = useState('light');
+
+	// const toggleTheme = () => {
+	// 	if (theme === 'light') {
+	// 		setTheme('dark');
+	// 	} else {
+	// 		setTheme('light');
+	// 	}
+	// };
+	const [ darkMode, setDarkMode ] = React.useState(getInitialMode());
+	React.useEffect(
+		() => {
+			localStorage.setItem('dark', JSON.stringify(darkMode));
+		},
+		[ darkMode ]
+	);
+
+	function getInitialMode() {
+		const isReturningUser = 'dark' in localStorage;
+		const savedMode = JSON.parse(localStorage.getItem('dark'));
+		const userPrefDarkScheme = getPrefColorScheme();
+
+		if (isReturningUser) {
+			//if mode was saved return dark / light
+			return savedMode;
+		} else if (userPrefDarkScheme) {
+			//if preferred scheme is dark - return dark
+			return true;
+		} else {
+			//otherwise - return light scheme
+			return false;
+		}
+		// return savedMode || false;
+	}
+
+	function getPrefColorScheme() {
+		if (!window.matchMedia) return;
+		return window.matchMedia('(prefers-color-scheme: dark)').matches;
+	}
+
+	return (
+		<Router>
+			{/* <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}> */}
+			<div className="main-app-container">
+				<div className={darkMode ? 'dark-mode' : 'light-mode'}>
+					{/* <h1>{darkMode ? 'Dark Mode' : 'Light Mode'}</h1> */}
+					{/* <> */}
+
+					{/* </> */}
+					{/* <GlobalStyles /> */}
 					<Nav />
+					<nav>
+						{/* toggle goes here */}
+						<div className="toggle-container">
+							<button onClick={() => setDarkMode((prevMode) => !prevMode)}>toggle mode</button>
+						</div>
+					</nav>
+					<h1>{darkMode ? 'Dark Mode' : 'Light Mode'}</h1>
+					{/* <button onClick={toggleTheme}>Toggle theme</button>
+					<h1>It's a light theme!</h1> */}
+					{/* <footer /> */}
 					<Switch>
 						<Route exact path="/" component={Home} />
 						<Route exact path="/Home" component={Home} />
@@ -27,10 +90,13 @@ class App extends React.Component {
 						<Route exact path="/404" component={NotFoundPage} />
 						<Redirect to="/404" />
 					</Switch>
+
 					<Footer />
 				</div>
-			</Router>
-		);
-	}
+			</div>
+			{/* </ThemeProvider> */}
+		</Router>
+	);
 }
+// }
 export default App;
