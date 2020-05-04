38d3f8b66632d4d30deffbc6414c987636999ecd
Nisha <nishavijai@gmail.com>
2020-05-04 19:28:50 +0200

    UPDATE LOGO JS

--- src/assets/components/pages/Nav/Logo/Logo.js
+++ src/assets/components/pages/Nav/Logo/Logo.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import './logo.css';
-
 import { Link } from 'react-router-dom';
+
+import './logo.css';
 export default class Logo extends Component {
 	render() {
 		return (
 			<div>
 				<div className="logo-div">
-					{/* <div className="logo" /> */}
 					<Link className="menu-link-li menu-link-nav" to="/Home">
 						<li>
 							<div className="logo" />
