719eeac6b81f4bc7105a7488449a8441f809cdae
Nisha <nishavijai@gmail.com>
2020-05-04 19:21:56 +0200

    REMOVE HEADER COMPONENT AND UPDATE HOME COMPONENT

--- src/assets/components/pages/Home/Home.js
+++ src/assets/components/pages/Home/Home.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import Header from '../Header/Header';
+import './home.css';
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div>
-				<div className="home-container">
-					<Header />
-				</div>
+				<header className="showcase">
+					<div className="container showcase-inner">
+						<h1>Front-End Developer</h1>
+						<p>Recent Projects</p>
+						<button className="btn">
+							<img src={require('../../../images/down-arrow.png')} alt="" />
+						</button>
+					</div>
+				</header>
 			</div>
 		);
 	}
