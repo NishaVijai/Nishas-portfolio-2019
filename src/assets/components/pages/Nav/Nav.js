7beffaa035b26b646c2d5c8099becdf24bb3ef6a
Nisha <nishavijai@gmail.com>
2020-05-04 19:40:21 +0200

    UPDATE NAV JS
    Update nav js by removing unused codes inside comment lines

--- src/assets/components/pages/Nav/Nav.js
+++ src/assets/components/pages/Nav/Nav.js
@@ -1,36 +1,14 @@
 import React from 'react';
 import Logo from './Logo/Logo';
-// import TopMenu from './TopMenu/TopMenu';
-// import MenuCss from './MenuCSS/MenuCss';
-// import { Link } from 'react-router-dom';
 import Dropdown from './Dropdown';
 
 import './nav.css';
 
 function Nav() {
-	// let links = [
-	// 	{ label: 'Home', link: '#home' },
-	// 	{ label: 'About', link: '#about' },
-	// 	{ label: 'Portfolio', link: '#portfolio' },
-	// 	{ label: 'Resume', link: '#resume' },
-	// 	{ label: 'Contact', link: '#contact' }
-	// ];
 	return (
 		<nav>
 			<Logo />
-			{/* <TopMenu /> */}
-			{/* <MenuCss /> */}
 			<Dropdown />
-			{/* <MenuCss links={links} /> */}
-
-			{/* <ul className="nav-links-new">
-				<Link to="/About" style={navStyle}>
-					<li>About</li>
-				</Link>
-				<Link to="/Shop" style={navStyle}>
-					<li>Shop</li>
-				</Link>
-			</ul> */}
 		</nav>
 	);
 }
