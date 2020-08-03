3a81dd3199a826033416ffff02a1b57963c1f64d
Nisha <nishavijai@gmail.com>
2020-08-03 14:02:12 +0200

    ADD NEW LAYOUT AND FOLDERS TO START

---
+++ src/assets/components/NewDesign/HeaderNewFolder/HeaderNew.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class HeaderNew extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Main Page</h1>
+
+        <nav>
+          <ul>
+            <li>portfolio</li>
+            <li>certificates</li>
+            <li>about</li>
+            <li>resume</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default HeaderNew;
---
+++ src/assets/components/NewDesign/NewLayout.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import HeaderNew from "../NewDesign/HeaderNewFolder/HeaderNew";
+export default class NewLayout extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <HeaderNew />
+        {/* <MainNew />
+                <FooterNew /> */}
+      </React.Fragment>
+    );
+  }
+}
