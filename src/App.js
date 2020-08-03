97de6343fc21fead4624ce827351cd55e07b3132
Nisha <nishavijai@gmail.com>
2020-08-03 19:04:42 +0200

    ADD APP CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import NewLayout from "./assets/components/NewDesign/NewLayout";
 
+import "./App.css";
+
 export default function App() {
   return (
     <div className="new-layout-container">
