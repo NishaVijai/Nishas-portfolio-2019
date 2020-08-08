b848f405a746cdec5da92cd4c0b99ef39ff26ccb
Nisha <nishavijai@gmail.com>
2020-08-08 14:58:01 +0200

    UPDATE NAVBAR COMPONENT

--- src/assets/components/NewDesign/NavBarFolder/NavBar.js
+++ src/assets/components/NewDesign/NavBarFolder/NavBar.js
@@ -5,9 +5,9 @@ export default class NavBar extends Component {
     return (
       <nav className="nav-bar-container">
         <ul className="nav-bar-container-list">
-          <li>portfolio</li>
-          <li>certificates</li>
           <li>about</li>
+          <li>portfolio</li>
+          {/* <li>certificates</li> */}
           <li>
             <a href="https://nishas-resume-2020.herokuapp.com/">Resume</a>
           </li>
