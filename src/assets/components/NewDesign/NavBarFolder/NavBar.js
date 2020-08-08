a9f520177f7f5e105f9359b64da9ae8230cc535e
Nisha <nishavijai@gmail.com>
2020-08-08 16:55:45 +0200

    MOVE NAVBAR CONTAINER INSIDE REACT FRAGMENT

--- src/assets/components/NewDesign/NavBarFolder/NavBar.js
+++ src/assets/components/NewDesign/NavBarFolder/NavBar.js
@@ -3,17 +3,20 @@ import React, { Component } from "react";
 export default class NavBar extends Component {
   render() {
     return (
-      <nav className="nav-bar-container">
+      <React.Fragment>
         <h3 className="sr-only">NavBar Container</h3>
-        <ul className="nav-bar-container-list">
-          <li>about</li>
-          <li>portfolio</li>
-          {/* <li>certificates</li> */}
-          <li>
-            <a href="https://nishas-resume-2020.herokuapp.com/">Resume</a>
-          </li>
-        </ul>
-      </nav>
+        <nav className="nav-bar-container">
+          {/* <h3 className="sr-only">NavBar Container</h3> */}
+          <ul className="nav-bar-container-list">
+            <li>about</li>
+            <li>portfolio</li>
+            {/* <li>certificates</li> */}
+            <li>
+              <a href="https://nishas-resume-2020.herokuapp.com/">Resume</a>
+            </li>
+          </ul>
+        </nav>
+      </React.Fragment>
     );
   }
 }
