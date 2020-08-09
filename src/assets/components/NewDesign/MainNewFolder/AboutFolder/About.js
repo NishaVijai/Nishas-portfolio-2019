8e63045775bc7960a89045bd16afe14c98cd82db
Nisha <nishavijai@gmail.com>
2020-08-09 15:17:41 +0200

    UPDATE ABOUT COMPONENT

--- src/assets/components/NewDesign/MainNewFolder/AboutFolder/About.js
+++ src/assets/components/NewDesign/MainNewFolder/AboutFolder/About.js
@@ -5,9 +5,6 @@ export default class About extends Component {
     return (
       <React.Fragment>
         <h3 className="sr-only">About Container</h3>
-        {/* <article>
-          <p>About me</p>
-        </article> */}
         <p>About me</p>
       </React.Fragment>
     );
