b5c463bda9694a38e08f1096bffdc7a124344bb3
Nisha <nishavijai@gmail.com>
2020-08-09 15:34:37 +0200

    ADD LOGO

--- src/assets/components/NewDesign/HeaderNewFolder/HeaderNew.js
+++ src/assets/components/NewDesign/HeaderNewFolder/HeaderNew.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import NavBar from "../NavBarFolder/NavBar";
+
+import Logo from "../../../images/My-logo.png";
 class HeaderNew extends Component {
   render() {
     return (
       <header className="header-container">
         <h2 className="sr-only">Header</h2>
+        <img src={Logo} alt="Site Logo" height="120" width="120" />
         <nav>
           <h3 className="sr-only">NavBar Container</h3>
           <NavBar />
