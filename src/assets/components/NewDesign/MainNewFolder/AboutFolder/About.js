c641145123a875df1b0df8a55678fbaacc050ae6
Nisha <nishavijai@gmail.com>
2020-08-09 20:13:06 +0200

    UPDATE ARTICLE TAG

--- src/assets/components/NewDesign/MainNewFolder/AboutFolder/About.js
+++ src/assets/components/NewDesign/MainNewFolder/AboutFolder/About.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 export default class About extends Component {
   render() {
     return (
-      <React.Fragment>
+      <article id="about">
         <h3 className="sr-only">About Container</h3>
         <p>About me</p>
-      </React.Fragment>
+      </article>
     );
   }
 }
