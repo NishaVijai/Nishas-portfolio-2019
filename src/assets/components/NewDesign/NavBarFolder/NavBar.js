59630135d511196145cedd85e8bec0441fa56ddd
Nisha <nishavijai@gmail.com>
2020-08-09 20:12:04 +0200

    ADD ID TO A HREF

--- src/assets/components/NewDesign/NavBarFolder/NavBar.js
+++ src/assets/components/NewDesign/NavBarFolder/NavBar.js
@@ -4,10 +4,20 @@ export default class NavBar extends Component {
   render() {
     return (
       <ul className="nav-bar-container-list">
-        <li>about</li>
-        <li>portfolio</li>
         <li>
-          <a href="https://nishas-resume-2020.herokuapp.com/">Resume</a>
+          <a href="#about">about</a>
+        </li>
+        <li>
+          <a href="#portfolio">portfolio</a>
+        </li>
+        <li>
+          <a
+            href="https://nishas-resume-2020.herokuapp.com/"
+            rel="noopener  noreferrer"
+            target="_blank"
+          >
+            Resume
+          </a>
         </li>
       </ul>
     );
