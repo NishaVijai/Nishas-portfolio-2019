60789934306fd8b62f8e08a1241550b49c6fb850
Nisha <nishavijai@gmail.com>
2020-08-09 15:20:58 +0200

    UPDATE HEADERNEW COMPONENT

--- src/assets/components/NewDesign/HeaderNewFolder/HeaderNew.js
+++ src/assets/components/NewDesign/HeaderNewFolder/HeaderNew.js
@@ -9,18 +9,6 @@ class HeaderNew extends Component {
           <h3 className="sr-only">NavBar Container</h3>
           <NavBar />
         </nav>
-
-        {/* <nav className="nav-bar-container">
-          <h2 className="sr-only">NavBar Container</h2>
-          <ul className="nav-bar-container-list">
-            <li>about</li>
-            <li>portfolio</li>
-            {/* <li>certificates</li> */}
-        {/* <li>
-              <a href="https://nishas-resume-2020.herokuapp.com/">Resume</a>
-            </li>
-          </ul>
-        </nav> */}
       </header>
     );
   }
