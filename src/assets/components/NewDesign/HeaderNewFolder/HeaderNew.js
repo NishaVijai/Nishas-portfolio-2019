aeae11fc568c4ce2f512ea967ce90859c9afd368
Nisha <nishavijai@gmail.com>
2020-08-10 12:49:17 +0200

    ADD MOBILE MEDIA QUERY

--- src/assets/components/NewDesign/HeaderNewFolder/HeaderNew.js
+++ src/assets/components/NewDesign/HeaderNewFolder/HeaderNew.js
@@ -8,7 +8,7 @@ class HeaderNew extends Component {
       <header id="header" className="header-container">
         <h2 className="sr-only">Header</h2>
         <a href="#header">
-          <img src={Logo} alt="Site Logo" height="120" width="120" />
+          <img src={Logo} alt="Site Logo" />
         </a>
         <nav>
           <h3 className="sr-only">NavBar Container</h3>
