efe1150a07f2a986a0b306e61c527f89713e3d0d
Nisha <nishavijai@gmail.com>
2020-08-12 21:15:36 +0200

    ADD ABOUT ME CONTENT

--- src/assets/components/NewDesign/MainNewFolder/AboutFolder/About.js
+++ src/assets/components/NewDesign/MainNewFolder/AboutFolder/About.js
@@ -6,6 +6,14 @@ export default class About extends Component {
       <article id="about">
         <h3 className="sr-only">About Container</h3>
         <p>About me</p>
+        <p>
+          A self-motivated, creative, and ambitious web developer from Denmark
+        </p>
+        <p>
+          I have done my Masters degree in Computer Applications from India. For
+          the past several months I have been building responsive websites using
+          React, Semantic HTML, CSS, and JavaScript.
+        </p>
       </article>
     );
   }
