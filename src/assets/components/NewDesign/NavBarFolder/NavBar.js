8101a9d4060385193b6ef9a3329c06c03b672196
Nisha <nishavijai@gmail.com>
2020-08-17 11:42:19 +0200

    DELETE COMMENTED LI ANCHORS

--- src/assets/components/NewDesign/NavBarFolder/NavBar.js
+++ src/assets/components/NewDesign/NavBarFolder/NavBar.js
@@ -77,26 +77,6 @@ export default class NavBar extends Component {
               </a>
             </li>
           </ul>
-          {/* <ul className="nav-bar-container-list">
-            <li>
-              <a href="#about">about</a>
-            </li>
-            <li>
-              <a href="#portfolio">portfolio</a>
-            </li>
-            <li>
-              <a href="#certificate">certificates</a>
-            </li>
-            <li>
-              <a
-                href="https://resume.nishavijai.vercel.app/"
-                rel="noopener  noreferrer"
-                target="_blank"
-              >
-                Resume
-              </a>
-            </li>
-          </ul> */}
         </nav>
       </section>
     );
