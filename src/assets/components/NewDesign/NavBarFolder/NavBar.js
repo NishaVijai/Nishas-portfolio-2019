b2f44b547d5182828094aced03fedc1cc7e25147
Nisha <nishavijai@gmail.com>
2020-08-20 10:49:09 +0200

    REMOVE OLD UNWRAPPED LINK

--- src/assets/components/NewDesign/NavBarFolder/NavBar.js
+++ src/assets/components/NewDesign/NavBarFolder/NavBar.js
@@ -20,54 +20,6 @@ export default class NavBar extends Component {
         <nav>
           <h4 className="sr-only">NavBar Items</h4>
           <ul className="nav-bar-container-list">
-            {/* <Link
-              aria-label="About section"
-              title="Scroll to About Section"
-              tabIndex="0"
-              className="link-item"
-              onKeyPress={scrollToAbout}
-              activeClass="active"
-              to="about"
-              spy={true}
-              smooth={true}
-              offset={-120}
-              duration={1000}
-            >
-              About
-            </Link>
-
-            <Link
-              aria-label="Portfolio section"
-              title="Scroll to Portfolio Section"
-              tabIndex="0"
-              className="link-item"
-              onKeyPress={scrollToPortfolio}
-              activeClass="active"
-              to="portfolio"
-              spy={true}
-              smooth={true}
-              offset={-120}
-              duration={1000}
-            >
-              Portfolio
-            </Link>
-
-            <Link
-              aria-label="Certificates section"
-              title="Scroll to Certificates Section"
-              tabIndex="0"
-              className="link-item"
-              onKeyPress={scrollToCertificates}
-              activeClass="active"
-              to="certificate"
-              spy={true}
-              smooth={true}
-              offset={-120}
-              duration={1000}
-            >
-              Certificates
-            </Link> */}
-
             <li>
               <Link
                 aria-label="About section"
