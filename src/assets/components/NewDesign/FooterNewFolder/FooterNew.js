d78c7d57e96a36a3d7c94bab34c829bc007c150c
NishaVijai <nishavijai@gmail.com>
2023-01-09 11:48:09 +0100

    Update footer copyright

--- src/assets/components/NewDesign/FooterNewFolder/FooterNew.js
+++ src/assets/components/NewDesign/FooterNewFolder/FooterNew.js
@@ -4,14 +4,10 @@ import SocialMedia from "./SocialMediaLinks/SocialMedia";
 
 export default class FooterNew extends Component {
   render() {
-    const getCurrentYearFn = () => {
-      console.log("test");
-      document.write(new Date().getFullYear())
-    };
     return (
       <footer className="footer-container">
         <h2 className="sr-only">Footer</h2>
-        <p>&copy; 2020 - {getCurrentYearFn}</p>
+        <p>&copy; 2020 - 2023</p>
         <SocialMedia />
       </footer>
     );
